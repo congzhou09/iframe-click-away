37d0c53fe296cd0383a172eba11e5604cbce2d37
congzhou09 <yuancongzhou@yeah.net>
2025-03-17 10:56:25 +0800

    feat: refine code and README

---
+++ README.md
@@ -0,0 +1,51 @@
+## iframe-click-away
+
+◇ Most tooltip and popup tools has a "click away" feature, which allows users to close a tooltip, popup, or modal by clicking anywhere outside of it.
+
+◇ When there's a iframe in page, "click away" won't work between regions in outer page and in inner iframe.
+
+◇ This tool is used to let "click away" work in the above scenario, with less codes.
+
+◇ Mechanism: when a click occurs on one side, a mimetic click will be triggered on the other side at (clientX, clientY), which defaults to (2,2).
+
+## usage
+
+◆ Install this npm dependency.
+
+◆ On the outer side:
+
+```
+import { Outer } from 'iframe-click-away';
+
+// get iframe window
+const iframeWindow = iframe.contentWindow;
+
+// provide info about inner iframe and outer page, and outer optional mimetic click position.
+const outer = new Outer(iframeWindow, window /*,clientX, clientY*/);
+
+// init
+outer.init();
+
+// invoke deinit() when destroying process is needed
+outer.deinit();
+
+```
+
+◆ On the inner side:
+
+```
+import { Inner } from 'iframe-click-away';
+
+// get outer page's window
+const outerWindow = window.parent;
+
+// provide info about inner iframe and outer page, and inner optional mimetic click position.
+const inner = new Inner(window, outerWindow /*,clientX, clientY*/);
+
+// init
+inner.init();
+
+// invoke deinit() when destroying process is needed
+inner.deinit();
+
+```
--- src/inner.ts
+++ src/inner.ts
@@ -1,38 +1,41 @@
 import { INTER_MSG } from "./constant";
+import { log } from "./util";
 
 export class Inner {
+  private clickEventMap = new WeakMap();
   constructor(
-    private readonly clickEventMap = new WeakMap(),
     private readonly innerWindow: Window,
-    private readonly outerWindow: Window
+    private readonly outerWindow: Window,
+    private readonly clientX: number = 2,
+    private readonly clientY: number = 2
   ) {}
 
-  private onInnerClick(e: MouseEvent) {
+  private onInnerClick = (e: MouseEvent) => {
     if (!this.clickEventMap.has(e) && this.outerWindow) {
-      console.debug("inner click");
+      log("inner click");
       this.outerWindow.postMessage({ message: INTER_MSG.innerClick }, "*");
     } else {
-      console.debug("inner click from outer msg");
+      log("inner click from outer msg");
     }
-  }
+  };
 
-  private onOuterClickMsg(e: MessageEvent) {
+  private onOuterClickMsg = (e: MessageEvent) => {
     if (e.data.message === INTER_MSG.outerClick) {
-      console.debug("get outerClick msg");
+      log("get outerClick msg");
       const element = this.innerWindow.document.elementFromPoint(2, 2);
       const events = ["mousedown", "mouseup", "click"];
       events.forEach((eventType) => {
         const event = new MouseEvent(eventType, {
           bubbles: true,
           cancelable: true,
-          clientX: 2,
-          clientY: 2,
+          clientX: this.clientX,
+          clientY: this.clientY,
         });
         this.clickEventMap.set(event, true);
         element?.dispatchEvent(event);
       });
     }
-  }
+  };
 
   init() {
     this.innerWindow.document.addEventListener(
